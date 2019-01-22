f953f1ba19ebfb63ec37cc36b2c5f075c4830768
jxom <jakemoxey@gmail.com>
2019-01-22 13:13:19 +1100

    Fixup tests

--- index.test.js
+++ index.test.js
@@ -1,25 +1,25 @@
-const validateProviderNumber = require('./index');
+const validateMedicareNumber = require('./index');
 
 test('throws if the Medicard card number is not provided', () => {
-  expect(() => validateProviderNumber()).toThrowError();
+  expect(() => validateMedicareNumber()).toThrowError();
 });
 
 test('throws if the Medicard card number is not a string', () => {
-  expect(() => validateProviderNumber(123)).toThrowError();
+  expect(() => validateMedicareNumber(123)).toThrowError();
 });
 
 test('returns false for an invalid Medicare card number (has alphabetic character)', () => {
-  expect(validateProviderNumber('95050739A')).toBeFalsy();
+  expect(validateMedicareNumber('95050739A')).toBeFalsy();
 });
 
 test('returns false for an invalid Medicare card number (length not equal to 10)', () => {
-  expect(validateProviderNumber('695050739')).toBeFalsy();
+  expect(validateMedicareNumber('695050739')).toBeFalsy();
 });
 
 test('returns false for an invalid Medicare card number (incorrect check digit)', () => {
-  expect(validateProviderNumber('6950507381')).toBeFalsy();
+  expect(validateMedicareNumber('6950507381')).toBeFalsy();
 });
 
 test('returns true for a valid Medicare card number', () => {
-  expect(validateProviderNumber('6950507391')).toBeTruthy();
+  expect(validateMedicareNumber('6950507391')).toBeTruthy();
 });
